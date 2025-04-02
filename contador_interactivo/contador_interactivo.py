dfeb8814a4902158cfc2e9c6ff2c65638edb6242
Abel Gomez Mendez <abelmetaltele@gmail.com>
2025-04-02 11:32:07 -0400

    Commit previo al primer push

--- contador_interactivo/contador_interactivo.py
+++ contador_interactivo/contador_interactivo.py
@@ -6,34 +6,81 @@ from rxconfig import config
 
 
 class State(rx.State):
-    """The app state."""
+    """
+    Representa el estado de la aplicación.
+
+    Atributos:
+        contador (int): Un contador que almacena un valor numérico que puede ser incrementado o decrementado
+    """
+    contador:int = 0
+
+    def incrementar(self) -> None:
+        """
+        Incrementa el valor del contador en 1.
+
+        Este método se utiliza como manejador de eventos para actualizar el estado interno 
+        cuando se requiere aumentar el valor del contador.
+        """
+        self.contador += 1
+
+
+    def decrementar(self) -> None:
+        """
+        Decrementa el valor del contador en 1.
+
+        Este método se utiliza como manejador de eventos para actualizar el estado interno 
+        cuando se requiere disminuir el valor del contador.
+        """
+        self.contador -= 1
 
     ...
 
-
 def index() -> rx.Component:
-    # Welcome Page (Index)
+    """
+    Construye y retorna el componente principal de la página de bienvenida de la aplicación.
+
+    La función index crea un contenedor que agrupa varios componentes de la interfaz:
+      - Un botón para cambiar el modo de color, ubicado en la parte superior derecha.
+      - Un centro que contiene un conjunto vertical de elementos:
+          • Un encabezado con el título "Contador Interactivo".
+          • Un texto descriptivo que instruye al usuario para incrementar o decrementar el contador.
+          • Un botón "Incrementar" que, al hacer clic, ejecuta el método State.incrementar.
+          • Un encabezado que muestra el valor actual del contador.
+          • Un botón "Decrementar" que, al hacer clic, ejecuta el método State.decrementar.
+    
+    Returns:
+        rx.Component: El componente que representa la vista principal de la aplicación.
+    """
     return rx.container(
         rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
+        rx.center(
+            rx.vstack(
+                rx.heading("Contador Interactivo", size="9"),
+                rx.text(
+                "Incremente o decremente el valor haciendo clic en los respectivos botones",
                 size="5",
+                justify="center",
+                ),
+                rx.button(
+                "Incrementar",
+                color_scheme = "green",
+                on_click = State.incrementar
+                ),
+                rx.heading(
+                State.contador,
+                font_size = "2em"
+                ),
+                rx.button(
+                "Decrementar",
+                color_scheme = "red",
+                on_click = State.decrementar              
+                ),
+                spacing="5",
+                justify="center",
+                min_height="85vh",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+        )
     )
-
 
 app = rx.App()
 app.add_page(index)
